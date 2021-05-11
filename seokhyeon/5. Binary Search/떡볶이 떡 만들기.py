77ae9fab6973e1463666ef842bf8b48d15c3d77a
h0ho-hong <rockthread1234@gmail.com>
2021-05-12 00:16:43 +0900

    complete

--- seokhyeon/5. Binary Search/떡볶이 떡 만들기.py
+++ seokhyeon/5. Binary Search/떡볶이 떡 만들기.py
@@ -1,3 +1,5 @@
+'''주어진 리스트에 너무 매몰될 필요는 없다.'''
+
 # 떡 개수, 요청한 떡의 길이
 n, m = map(int, input().split())
 
@@ -8,20 +10,24 @@ rc_array.sort()
 
 start = 0
 end = rc_array[-1]
+result = 0
 
 while start <= end:
     cut_length = 0
     mid = (start + end) // 2
     for length in rc_array:
-        if length >= rc_array[mid]:
-            cut_length += length - rc_array[mid]
+        if length >= mid:
+            cut_length += length - mid
 
     if m == cut_length:
-        print(mid)
+        result = mid
         break
     elif m > cut_length:
         end = mid - 1
     elif m < cut_length:
         start = mid + 1
+        result = max(mid, result) # 적어도 양 맞추기 = 최대한 떡 자르기
+
+print(result)
 
 
