0124530e049b6213e6bff49e4e2fc81dbc32bed0
강수민 <gsmin2018@gmail.com>
2021-08-02 14:58:17 +0000

    백준 문제풀이 수정

--- sumin/3. DFS, BFS/Baekjoon PS/[7576]tomato_upgrade.py
+++ sumin/3. DFS, BFS/Baekjoon PS/[7576]tomato_upgrade.py
@@ -2,41 +2,28 @@ import sys
 from collections import deque
 r = sys.stdin.readline
 
+m, n = map(int, input().rstrip().split())
+box = []
+time = -1
+point_list = deque()
 
-def aaaasdaf(M, N, box):
-    # 좌우상하
-    dx = [0, 0, 1, -1]
-    dy = [-1, 1, 0, 0]
-
-    days = -1
-
-    while wow:
-        days += 1
-        for _ in range(len(wow)):
-            x, y = wow.popleft()
-
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-
-                if (0 <= nx < N) and (0 <= ny < M) and (box[nx][ny] == 0):
-                    box[nx][ny] = box[x][y] + 1
-                    wow.append([nx, ny])
-
-    for b in box:
-        if 0 in b:
-            return -1
-    return days
-
-
-M, N = map(int, r().split())
-box, wow = [], deque()
-for i in range(N):
-    row = list(map(int, r().split()))
-    for j in range(M):
+for i in range(n):
+    row = list(map(int, input().split()))
+    for j in range(m):
         if row[j] == 1:
-            wow.append([i, j])
+            point_list.append([i, j])
     box.append(row)
 
+while point_list:
+    now_turn = len(point_list)
+    for i in range(now_turn):
+        row, col = point_list.popleft()
+        direc = [[row + 1, col], [row - 1, col], [row, col + 1], [row, col - 1]]
+        for r, c in direc:
+            if 0 <= r < n and 0 <= c < m and box[r][c] == 0:
+                point_list.append([r, c])
+        if i == now_turn - 1:
+            time += 1
 
-print(aaaasdaf(M, N, box))
+print(time)
+
