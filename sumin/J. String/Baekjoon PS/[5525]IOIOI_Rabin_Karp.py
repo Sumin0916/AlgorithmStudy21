e864c4f2e89128b0a7ea25b79d340ccd12ac2188
강수민 <gsmin2018@gmail.com>
2021-08-08 14:59:12 +0000

    백준 문풀 추가

--- sumin/J. String/Baekjoon PS/[5525]IOIOI_Rabin_Karp.py
+++ sumin/J. String/Baekjoon PS/[5525]IOIOI_Rabin_Karp.py
@@ -1,26 +1,25 @@
 import sys
 
+
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-s = list(map(ord, list(input())))
-s.pop()
-res = 0
-dn = 2*n
+s = list(map(ord, list(input().rstrip())))
+pn_len = 2*n+1
+pn = "I"+"OI"*n
 want_hash = 0
-for i in range(1, dn+1, 2):
-    want_hash += 73 * (2 ** (i-1))
-    want_hash += 79 * (2 ** i)
-want_hash += 73 * (2 ** (dn))
-new_hash = 0
-for i in range(1, dn+1, 2):
-    new_hash += s[i-1] * (2 ** (i-1))
-    new_hash += s[i] * (2 ** i)
-if new_hash == want_hash:
+for i in range(pn_len):
+    want_hash += (2 ** i) * ord(pn[i])
+now_hash = 0
+for i in range(pn_len):
+    now_hash += (2 ** i) * s[i]
+res = 0
+if now_hash == want_hash:
     res += 1
-for i in range(dn+1, m-(dn)):
-    new_hash = ((new_hash - s[i-dn]) // 2) + s[i] * (2 ** i)
-    if new_hash == want_hash:
+for i in range(pn_len, m):
+    now_hash = (now_hash - s[i-(pn_len)])//2 + (s[i] * (2 ** (pn_len-1)))
+    if now_hash == want_hash:
         res += 1
+        i += 2
 print(res)
