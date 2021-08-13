8241c690d74ebdbd83b9771ec42f2b3492c4ef99
Sumin0916 <gsmin2018@gmail.com>
2021-08-13 16:34:49 +0900

    문제풀이 오류 수정

--- sumin/G. Shortest Path/Baekjoon PS/[2178]labyrinth_navigation.py
+++ sumin/G. Shortest Path/Baekjoon PS/[2178]labyrinth_navigation.py
@@ -1,98 +1,33 @@
+import collections
+
+
+point_lst = collections.deque()
+
 n, m = map(int, input().split())
 maze = [list(map(int, list(input()))) for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+#  1 == 움직일 수 있음, 0 == 움직일 수 없음
+point_lst.append([0, 0])
+count = 1
+direc = [(1, 0), (0, 1), (0, -1), (-1, 0)]
 
-
-def can_move(array, i, j):
-    global n, m
-    if 0 <= i < n and 0 <= j < m and array[i][j]:
-        return [i, j]
-    else:
-        return False
-
-
-def navigate(array):
-    lst = array
-    RIGHT, DOWN, UP, LEFT = 0, 1, 2, 3
-    res = 1
-    row, col = 0, 0
-    mem = UP  # 이번 수행에서는 위로 올라가지 말라는 이야기
-    haha = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-    while True:
-        move = False
-        if row == n-1 and col == m-1:
-            return res
-        direc = [can_move(lst, row, col+1),
-                 can_move(lst, row+1, col),
-                 can_move(lst, row-1, col),
-                 can_move(lst, row, col-1)]
-
-        for i in range(4):
-            if i == mem:
-                continue
-            elif direc[i]:
-                row, col = direc[i][0], direc[i][1]
-                # print(i, row, col)
-                res += 1
-                move = True
-                if i == 0:
-                    mem = 3
-                elif i == 1:
-                    mem = 2
-                elif i == 2:
-                    mem = 1
-                elif i == 3:
-                    mem = 0
-                break
-        if not move:
-            lst[row][col] = 0
-            row += haha[mem][0]
-            col += haha[mem][1]
-            res -= 1
-        # time.sleep(1)
-
-
-def navigate_r(array):
-    DOWN, RIGHT, UP, LEFT = 0, 1, 2, 3
-    lst = array
-    res = 1
-    row, col = 0, 0
-    mem = UP  # 이번 수행에서는 위로 올라가지 말라는 이야기
-    haha = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-    while True:
-        move = False
-        if row == n-1 and col == m-1:
-            return res
-        direc = [can_move(lst, row+1, col),
-                 can_move(lst, row, col+1),
-                 can_move(lst, row-1, col),
-                 can_move(lst, row, col-1)]
-
-        for i in range(4):
-            if i == mem:
-                continue
-            elif direc[i]:
-                row, col = direc[i][0], direc[i][1]
-                # print(i, row, col)
-                res += 1
-                move = True
-                if i == 0:
-                    mem = 2
-                elif i == 1:
-                    mem = 3
-                elif i == 2:
-                    mem = 0
-                elif i == 3:
-                    mem = 1
-                break
-        if not move:
-            lst[row][col] = 0
-            row += haha[mem][0]
-            col += haha[mem][1]
-            res -= 1
-        # time.sleep(1)
-
-
-
-print(min(navigate(maze), navigate_r(maze)))
-
-
+while True:
+    temp_lst = collections.deque()
+    while point_lst:
+        r, c = point_lst.popleft()
+        if (r, c) == (n-1, m-1):
+            print(count)
+            break
+        for a, b in direc:
+            tr = r+a
+            tc = c+b
+            if 0 <= tr < n and 0 <= tc < m:
+                if maze[tr][tc] == 1 and not visited[tr][tc]:
+                    visited[tr][tc] = True
+                    temp_lst.append([tr,tc])
+        #print(point_lst)
+    if (r, c) == (n-1, m-1):
+        break
+    #print(temp_lst)
+    point_lst = temp_lst
+    count += 1
