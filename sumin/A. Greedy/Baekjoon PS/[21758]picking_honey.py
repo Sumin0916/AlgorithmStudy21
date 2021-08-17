c14cc146f2f07889068929fb7fec3b39d998366a
Sumin0916 <gsmin2018@gmail.com>
2021-08-17 23:46:36 +0900

    백준 문풀 추가

--- sumin/A. Greedy/Baekjoon PS/[21758]picking_honey.py
+++ sumin/A. Greedy/Baekjoon PS/[21758]picking_honey.py
@@ -1,5 +1,18 @@
-# n = int(input())
-# honey = list(map(int, input().split()))
+n = int(input())
+honey = list(map(int, input().split()))
+s = []
+s.append(honey[0])
+res = 0
+for i in range(1,n):
+    s.append(s[i-1]+honey[i])
 
-b = "ABCD1"
-print(b[:-1])
+for i in range(1, n-1):
+    res = max(res, s[n-2]-honey[0]+honey[i])
+
+for i in range(1, n-2):
+    res = max(res, 2 * s[n-1] - s[i] - honey[0] - honey[i])
+
+for i in range(1, n-1):
+    res = max(res, 2 * s[i-1] + s[n-2] - s[i])
+
+print(res)
