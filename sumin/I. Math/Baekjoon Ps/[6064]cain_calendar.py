7a1e2bc76f0857f751962564a37ea08bac8927cf
강수민 <gsmin2018@gmail.com>
2021-08-29 09:54:15 +0000

    백준 문풀 추가

--- sumin/I. Math/Baekjoon Ps/[6064]cain_calendar.py
+++ sumin/I. Math/Baekjoon Ps/[6064]cain_calendar.py
@@ -4,17 +4,4 @@ input = sys.stdin.readline
 t = int(input())
 
 for _ in range(t):
-    m, n, x, y = map(int, input().split())
-    dif = abs(m-n)
-    if m > n:
-        res = n
-        tx, ty = y, y
-        
-    elif m < n:
-        res = m
-        tx, ty = x, x 
-    else:
-        if x == y:
-            print(x)
-        else:
-            print(-1)
+    
