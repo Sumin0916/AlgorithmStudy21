e986d75392ca15a040f13d0f58f7960a780a24c6
강수민 <gsmin2018@gmail.com>
2021-09-01 10:34:31 +0000

    백준 문제 풀이 Solved.ac class 3++ 완료

--- sumin/I. Math/Baekjoon Ps/[6064]cain_calendar.py
+++ sumin/I. Math/Baekjoon Ps/[6064]cain_calendar.py
@@ -1,25 +1,15 @@
-import sys
-
-input = sys.stdin.readline
-
-
-# def cain_calendar(month, day, g_month, g_day):
-    
-
-# year 를 10으로 나누면 3, 12로 나누면 9
-# 3, 13, 23, 33, 43, 54...
-# 9, 21, 33, 45 ...
-
-
 for _ in range(int(input())):
-    m, n, x, y = map(int, input().split())
-    if m < n:
-        cain_calendar(m, n, x, y)
-    elif m > n:
-        cain_calendar(n, m, y, x)
-    else:
-        if x != y:
-            print(-1)
-        else:
-            print(x)
-# 11 13 6 5
+    m, n, b, c = map(int, input().split())
+    stat = False
+    if b > c:
+        m, n = n, m
+        b, c = c, b
+    ny = c - b
+    while ny <= m * n:
+        if ny % m == 0:
+            print(b + ny)
+            stat = True
+            break
+        ny += n
+    if not stat:
+        print(-1)
