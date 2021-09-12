c2f39016597faee236531d060c55a621461dacec
강수민 <gsmin2018@gmail.com>
2021-09-12 14:29:39 +0000

    백준 문풀 추가 트리의 지름....

--- sumin/[1967]diameter_of_the_tree.py
+++ sumin/[1967]diameter_of_the_tree.py
@@ -2,9 +2,29 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-visited = [list(False for _ in range(n+1)) for _ in range(n+1)]
+N = int(input())
+INF = 10000001
+graph = [list() for _ in range(N+1)]
 res = -1
+for _ in range(N-1):
+    a, b, w = map(int, input().split())
+    graph[a].append([b, w])
+    graph[b].append([a, w])
 
+def dfs(start):
+    global res
+    stack = [[start, 0]]
+    visit = [False for _ in range(N+1)]
+    visit[start] = True
+    while stack:
+        node, weight = stack.pop()
+        if res < weight:
+            res = weight
+        for n, w in graph[node]:
+            if not visit[n]:
+                stack.append([n, weight + w])
+                visit[n] = True
+for i in range(1, N+1):
+    dfs(i)
 
-def dfs():
+print(res)
