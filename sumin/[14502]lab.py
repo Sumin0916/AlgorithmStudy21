2419f78a8b29daeb29f8420cdc83d08b033ba41d
강수민 <gsmin2018@gmail.com>
2021-09-18 08:05:20 +0000

    백준 문풀 추가

--- sumin/[14502]lab.py
+++ sumin/[14502]lab.py
@@ -1,5 +1,22 @@
-a = input()
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+queue = deque()
 res = 0
-for n in a:
-    res += int(n) ** 5
-print(res)
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+is_cons = [list(False for _ in range(M)) for _ in range(N)]
+
+direc = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 2:
+            queue.append([i, j])
+            is_cons[i][j] = True
+        elif graph[i][j] == 1:
+            is_cons[i][j] = True
+
+
