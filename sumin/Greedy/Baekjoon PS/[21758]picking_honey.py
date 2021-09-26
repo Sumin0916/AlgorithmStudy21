3f21c28d09742693c8173d5cfed259f030255bf2
강수민 <gsmin2018@gmail.com>
2021-09-26 11:26:15 +0000

    백준 문풀 추가

--- sumin/Greedy/Baekjoon PS/[21758]picking_honey.py
+++ sumin/Greedy/Baekjoon PS/[21758]picking_honey.py
@@ -3,16 +3,16 @@ honey = list(map(int, input().split()))
 s = []
 s.append(honey[0])
 res = 0
-for i in range(1,n):
+for i in range(1, n):
     s.append(s[i-1]+honey[i])
 
 for i in range(1, n-1):
-    res = max(res, s[n-2]-honey[0]+honey[i])
-
-for i in range(1, n-2):
-    res = max(res, 2 * s[n-1] - s[i] - honey[0] - honey[i])
+    res = max(res, s[i] - honey[0] + s[n-2] - s[i-1])  # 벌 집 벌
 
 for i in range(1, n-1):
-    res = max(res, 2 * s[i-1] + s[n-2] - s[i])
+    res = max(res, 2 * s[n-1] - s[i] - honey[0] - honey[i])  # 벌 벌 집
+
+for i in range(1, n-1):
+    res = max(res, s[n-2] + s[i-1] - honey[i])  # 집 벌 벌
 
 print(res)
--- sumin/[1043]lie.py
+++ sumin/[1043]lie.py
@@ -10,6 +10,7 @@ for _ in range(M):
     temp = temp[1:]
     party_lst.append(set(temp))
 
+
 def back(ind, count):
     global res, known
     if ind == M:
@@ -18,12 +19,12 @@ def back(ind, count):
         return
     inter = party_lst[ind] & known
 
-    if inter == set(): # 파티에 진실을 아는 사람이 없을 때
-        back(ind+1, count+1) # 거짓말
+    if inter == set():  # 파티에 진실을 아는 사람이 없을 때
+        back(ind+1, count+1)  # 거짓말
         known.union(inter)
-        back(ind+1, count) #진실
+        back(ind+1, count)  # 진실
         known.difference(inter)
-    else: # 진실을 아는 사람이 있을 때(무조건 진실을 이야기 한다.)
+    else:  # 진실을 아는 사람이 있을 때(무조건 진실을 이야기 한다.)
         known.union(inter)
         back(ind+1, count)
         known.difference(inter)
