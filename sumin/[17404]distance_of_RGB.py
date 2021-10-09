5de555bd14e8bdef083f169026fb378566970c44
Sumin Kang <gsmin2018@gmail.com>
2021-10-09 14:53:19 +0000

    백준 문풀 추가

--- sumin/[17404]distance_of_RGB.py
+++ sumin/[17404]distance_of_RGB.py
@@ -1,8 +1,11 @@
 import sys
 
 input = sys.stdin.readline
-dp = [list(1e10 for _ in range(3)) for _ in range(2)]
-N = int(input())
 
-for _ in range(N):
-    
+N = int(input())
+house_lst = list(list(map(int, input().split())) for _ in range(N))
+
+for i in range(2, N-1):
+    house_lst[i][0] = min(house_lst[i-1][1], house_lst[i-1][2]) + house_lst[i][0]
+    house_lst[i][1] = min(house_lst[i-1][0], house_lst[i-1][2]) + house_lst[i][1]
+    house_lst[i][2] = min(house_lst[i-1][0], house_lst[i-1][1]) + house_lst[i][2]
