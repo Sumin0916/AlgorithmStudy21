2c2ad49ea51746958c80561b0e38f08053a1df64
Sumin Kang <gsmin2018@gmail.com>
2021-10-12 14:55:33 +0000

    백준 문풀 추가

--- sumin/[9466]term_project.py
+++ sumin/[9466]term_project.py
@@ -1,39 +1,23 @@
-T = int(input())
+import sys
 
-
-def find_parent(parent, x, count):
-    if count > N:
-        return x
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x], count+1)
-    return parent[x]
-
-
-def union_parent(parent, a, b):
-    a = find_parent(parent, a, 0)
-    b = find_parent(parent, b, 0)
-    if a < b:
-        parent[b] = a
+def find_parent(parent, node, cal, start):
+    if node not in cal:
+        cal.add(node)
+    elif node == start:
+        return node  # 성공적인 사이클
     else:
-        parent[a] = b
+        return -1  # 엉뚱한 사이클
+    if parent[node] != node:
+        find_parent(parent, parent[node], cal, start)
 
+input = sys.stdin.readline
 
+T = int(input())
 for _ in range(T):
+    count = 0
     N = int(input())
-    student = [x for x in range(N+1)]
-    want_vote = list(map(int, input().split()))
-    want_vote.insert(0, 0)
-    res = set()
-    ans = 0
+    student = list(map(int, input().split()))
+    student.insert(0, 0)
+    non_team = set()
     for i in range(1, N+1):
-        a, b = i, want_vote[i]
-        if find_parent(want_vote, a, 0) != find_parent(want_vote, b, 0):
-            union_parent(want_vote, a, b)
-    for i in range(1, N+1):
-        if i not in res:
-            res.add(i)
-            ans += 1
-    print(ans)
-    print(want_vote)
-            
-        
+        print(i, find_parent(student, i, set(), i))
