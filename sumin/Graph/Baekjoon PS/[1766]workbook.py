9791a24513d0fe342c5cebf259f4bd03e2059d3f
Sumin Kang <gsmin2018@gmail.com>
2021-10-20 14:35:55 +0000

    백준 문풀 추가

--- sumin/Graph/Baekjoon PS/[1766]workbook.py
+++ sumin/Graph/Baekjoon PS/[1766]workbook.py
@@ -4,15 +4,13 @@ import heapq
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-degree = [1] * (N+1)
-graph = [[i+1] for i in range(N+1)]
-graph[N][0] = N
+degree = [0] * (N+1)
+graph = [list() for _ in range(N+1)]
 degree[1] = 0
 
 for _ in range(M):
     a, b = map(int, input().split())
     degree[b] += 1
-    degree[a] -= 1
     graph[a].append(b)
 
 heap = []
@@ -28,5 +26,3 @@ while heap:
         degree[i] -= 1
         if degree[i] == 0:
             heapq.heappush(heap, i)
-
-
