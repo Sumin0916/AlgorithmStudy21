f195c818e616fafc720eae0bf7c925d93a62b0be
Sumin Kang <gsmin2018@gmail.com>
2021-10-30 14:44:49 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[12100]2048_easy.py
+++ sumin/_Unsolved problems/[12100]2048_easy.py
@@ -1,87 +1,90 @@
 import sys
+import copy
 
 input = sys.stdin.readline
 
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
-direc_lst = ["up", "down", "left", "right"]
 ans = 0
 
 
-def rotating(direc, graph):
-    global N
-    new_graph = [list(0 for _ in range(N)) for _ in range(N)]
-    if direc == "left":
+def moving_line(direc):
+    global graph
+    if direc == 0:
         for i in range(N):
-            for j in range(N):
-                new_graph[N-j-1][i] = graph[i][j]
-    elif direc == "right":
-        for i in range(N):
-            for j in range(N):
-                new_graph[j][N-i-1] = graph[i][j]
-    return new_graph
-
-
-def moving_line(graph):
-    global N
-    for i in range(N):
-        visited = set()
-        for j in range(N-1, -1, -1):
-            now_num = graph[j][i]
-            graph[j][i] = 0
-            if now_num == 0:
-                continue
-            for k in range(1, N):
-                if j + k >= N:
-                    graph[j+k-1][i] = now_num
-                    break
-                if graph[j+k][i] == now_num:
-                    if j+k not in visited:
-                        visited.add(j+k)
-                        graph[j+k][i] = 2 * now_num
-                        break
+            ind = 0
+            for j in range(1, N):
+                if graph[j][i]:
+                    temp = graph[j][i]
+                    graph[j][i] = 0
+                    if graph[ind][i] == 0:
+                        graph[ind][i] = temp
+                    elif graph[ind][i] == temp:
+                        graph[ind][i] = 2 * temp
+                        ind += 1
                     else:
-                        graph[j+k-1][i] = now_num
-                        break
-                elif graph[j+k][i] != 0 and graph[j+k][i] != now_num:
-                    graph[j+k-1][i] = now_num
-                    break
+                        ind += 1
+                        graph[ind][i] = temp
+    if direc == 1:
+        for i in range(N):
+            ind = N-1
+            for j in range(N-2, -1, -1):
+                if graph[j][i]:
+                    temp = graph[j][i]
+                    graph[j][i] = 0
+                    if graph[ind][i] == 0:
+                        graph[ind][i] = temp
+                    elif graph[ind][i] == temp:
+                        graph[ind][i] = 2 * temp
+                        ind -= 1
+                    else:
+                        ind -= 1
+                        graph[ind][i] = temp
+    if direc == 2:
+        for i in range(N):
+            ind = 0
+            for j in range(1, N):
+                if graph[i][j]:
+                    temp = graph[i][j]
+                    graph[i][j] = 0
+                    if graph[i][ind] == 0:
+                        graph[i][ind] = temp
+                    elif graph[i][ind] == temp:
+                        graph[i][ind] = 2 * temp
+                        ind += 1
+                    else:
+                        ind += 1
+                        graph[i][ind] = temp
+    if direc == 3:
+        for i in range(N):
+            ind = N-1
+            for j in range(N-2, -1, -1):
+                if graph[i][j]:
+                    temp = graph[i][j]
+                    graph[i][j] = 0
+                    if graph[i][ind] == 0:
+                        graph[i][ind] = temp
+                    elif graph[i][ind] == temp:
+                        graph[i][ind] = 2 * temp
+                        ind -= 1
+                    else:
+                        ind -= 1
+                        graph[i][ind] = temp
 
-def slide_map(direc, graph):
-    if direc == "up":
-        graph = rotating("left", graph)
-        graph = rotating("left", graph)
-        moving_line(graph)
-        graph = rotating("left", graph)
-        graph = rotating("left", graph)
 
-    elif direc == "down":
-        moving_line(graph)
-
-    elif direc == "left":
-        graph = rotating("left", graph)
-        moving_line(graph)
-        graph = rotating("right", graph)
-
-
-    elif direc == "right":
-        graph = rotating("left", graph)
-        moving_line(graph)
-        graph = rotating("right", graph)
-    return graph
-
-def cal(graph, cnt):
-    global direc_lst, ans
-    if cnt == 6:
-        #for i in graph:
-            #print(*i)
-        #print()
+def cal(cnt):
+    global graph, ans
+    if cnt == 5:
         ans = max(ans, max(list(map(max, graph))))
         return
-    for i in range(4):
-        cal(slide_map(direc_lst[i], graph), cnt+1)
-    
 
+    temp_graph = copy.deepcopy(graph)
+    for i in range(4):
+        moving_line(i)
+        cal(cnt+1)
+        graph = copy.deepcopy(temp_graph)
+
+
+cal(0)
 
 print(ans)
-
