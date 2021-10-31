7ea00ae23022166e037c48abcadf6c188bee10d0
Sumin Kang <gsmin2018@gmail.com>
2021-10-31 13:28:57 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[13460]beads_escape_2.py
+++ sumin/_Unsolved problems/[13460]beads_escape_2.py
@@ -2,91 +2,94 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-direc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+direc_lst = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 N, M = map(int, input().split())
 graph = list(list(input()) for _ in range(N))
+ans = 100
 
-
-def moving_bids(direction, red_point, blue_point):
-    dr, dc = direc[direction]
+def tilt_board(direc, red_point, blue_point):
     red_row, red_col = red_point
     blue_row, blue_col = blue_point
-    red_stat, blue_stat = True, True
+    is_red_goal = False
+    is_red_finish, is_blue_finish = False, False
+    ndr, ndc = -1 * direc_lst[direc][0], -1 * direc_lst[direc][1]
 
-    while red_stat or blue_stat:
-        if red_stat:
-            nred_row = red_row+dr
-            nred_col = red_col+dc
-            if 0<= nred_row < N and 0 <= nred_col < M and graph[nred_row][nred_col] != "#":
-                if graph[nred_row][nred_col] == "O":
-                    return "END"
-                red_row = nred_row
-                red_col = nred_col
+    for i in range(1, max(N, M)):
+        dr, dc = i*direc_lst[direc][0], i*direc_lst[direc][1]
+        new_red_row, new_red_col = red_row + dr, red_col + dc
+        new_blue_row, new_blue_col = blue_row + dr, blue_col + dc
+        if not is_red_finish:
+            if graph[new_red_row][new_red_col] == "O":
+                is_red_goal = True4456645465456332.
+                0
+                .
+                is_red_finish = True
+
+            elif graph[new_red_row][new_red_col] == "#":
+                new_red_point = [new_red_row+ndr, new_red_col+ndc]
+                is_red_finish = True
+
+        if not is_blue_finish:
+            if graph[new_blue_row][new_blue_col] == "O":
+                return "fail"
+
+            elif graph[new_blue_row][new_blue_col] == "#":
+                new_blue_point = [new_blue_row+ndr, new_blue_col+ndc]
+                is_blue_finish = True
+
+        if is_red_finish and is_blue_finish:
+            break
+
+    if is_red_goal:
+        return "goal"
+    if new_red_point == new_blue_point:
+        if direc == 0:
+            if red_row > blue_row:
+                new_blue_point[0] -= 1
             else:
-                red_stat = False
-        if blue_stat:
-            nblue_row = blue_row+dr
-            nblue_col = blue_col+dc
-            if 0<= nblue_row < N and 0 <= nblue_col < M and graph[nblue_row][nblue_col] != "#":
-                if graph[nblue_row][nblue_col] == "O":
-                    return "RE"
-                blue_row = nblue_row
-                blue_col = nblue_col
+                new_red_point[0] -= 1
+        if direc == 1:
+            if red_row > blue_row:
+                new_red_point[0] += 1
             else:
-                blue_stat = False
-
-    if red_row == blue_row and red_col == blue_col:
-        if red_point[0] == blue_point[0]:
-            if red_point[1] > blue_point[1]:
-                if direction == 2:
-                    blue_col -= 1
-                elif direction == 3:
-                    red_col -= 1
+                new_blue_point[0] += 1
+        if direc == 2:
+            if red_col > blue_col:
+                new_blue_point[1] -= 1
             else:
-                if direction == 2:
-                    red_col -= 1
-                elif direction == 3:
-                    blue_col -= 1
-        elif red_point[1] == blue_point[1]:
-            if red_point[0] > blue_point[0]:
-                if direction == 0:
-                    blue_col -= 1
-                elif direction == 1:
-                    red_col -= 1
+                new_red_point[1] -= 1
+        if direc == 3:
+            if red_col> blue_col:
+                new_red_point[1] += 1
             else:
-                if direction == 0:
-                    red_col -= 1
-                elif direction == 1:
-                    blue_col -= 1
+                new_blue_point[1] += 1
+    return [new_red_point, new_blue_point]
 
-    return (red_row, red_col), (blue_row, blue_col)
-
-
-def bfs():
-    count = 0
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == "R":
-                red_point = (i, j)
-            elif graph[i][j] == "B":
-                blue_point = (i, j)
-    point_queue = deque()
-    point_queue.append((red_point, blue_point))
-
-    while point_queue:
-        count += 1
-        while point_queue:
-            red_point, blue_point = point_queue.popleft()
+def bfs(queue):
+    cnt = 1
+    while queue and cnt < 11:
+        temp_queue = deque()
+        while queue:
+            rp, bp = queue.popleft()
             for i in range(4):
-                res = moving_bids(i, red_point, blue_point)
-                if res == "END":
-                    return count
-                elif res == "RE":
-                    pass
-                else:
-                    point_queue.append(tuple(res))
+                res = tilt_board(i, rp, bp)
+                if res == "goal":
+                    return cnt
+                elif res != "fail":
+                    temp_queue.append(res)
+        queue = temp_queue
+        cnt += 1
+    return -1
+    
+        
+queue = deque()
+for i in range(1, N-1):
+    for j in range(1, M-1):
+        if graph[i][j] == "R":
+            rp = [i, j]
+        elif graph[i][j] == "B":
+            bp = [i, j]
+queue.append([rp, bp])
 
-print(bfs())
-
-
+print(bfs(queue))
