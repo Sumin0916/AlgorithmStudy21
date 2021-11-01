28f3d53b499be7f8ac09d91bbab5acc39700e2ee
Sumin Kang <gsmin2018@gmail.com>
2021-11-01 14:46:42 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[1799]bishop.py
+++ sumin/_Unsolved problems/[1799]bishop.py
@@ -1,53 +1,37 @@
 import sys
 
-
-def put_it(row, col):
-    global N, graph
-    for i in range(1, row+1):
-        if 0 <= col+i < N:
-            if graph[row-i][col+i] == -1:
-                return False
-        if 0 <= col-i < N:
-            if graph[row-i][col-i] == -1:
-                return False
-    for i in range(1, N-row):
-        if 0 <= col+i < N:
-            if graph[row+i][col+i] == -1:
-                return False
-        if 0 <= col-i < N:
-            if graph[row+i][col-i] == -1:
-                return False
-    graph[row][col] = -1
-    return True
-    
-def find_ans(len_place, ind, count):
-    global graph, visited, res
-    if ind == len_place:
-        res = max(res, count)
-        return
-    for i in range(ind, len_place):
-        if not visited[i]:
-            row, col = placable[i] 
-            visited[i] = True
-            if put_it(row, col):
-                graph[row][col] = -1
-                count += 1
-            find_ans(len_place, ind+1, count)
-            graph[row][col] = 0
-            visited[i] = False
-
-
-
 N = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
-placable = []
-res = 0
+board = list(list(map(int, input().split())) for _ in range(N))
+ans = 0
+BISHOP = -1
+
+def can_place(row, col):
+    for r, c in bishop_lst:
+        if col != c and row != r:
+            if abs((row-r)/(col-c)) == 1:
+                return False
+    return True
+
+
+def simulator(order, cnt):
+    global board, ans, bishop_lst
+    for i in range(order, lst_len):
+        row, col = can_place_lst[i]
+        if can_place(row, col):
+            bishop_lst.append([row, col])
+            simulator(order+1, cnt+1)
+            bishop_lst.pop()
+        simulator(order+1, cnt)
+    ans = max(ans, cnt)
+
+can_place_lst = []
+bishop_lst = []
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1:
-            placable.append([i, j])
+        if board[i][j] == 1:
+            can_place_lst.append([i, j])
+lst_len = len(can_place_lst)
+print(can_place_lst)
+simulator(0, 0)
+print(ans)
 
-len_place = len(placable)
-visited = [False] * len_place
-print(put_it(1, 1))
-print(put_it(4, 4))
--- sumin/_Unsolved problems/[2143]sum_of_two_array.py
+++ sumin/_Unsolved problems/[2143]sum_of_two_array.py
@@ -1,8 +1,8 @@
 T = int(input())
 N = int(input())
-a_ary = list(map(int, input().split()))
+a_ary = [0] + list(map(int, input().split()))
 M = int(input())
-b_ary = list(map(int, input().split()))
+b_ary = [0] + list(map(int, input().split()))
 res = 0
 
 for i in range(1, N):
@@ -11,5 +11,15 @@ for i in range(1, M):
     b_ary[i] += b_ary[i-1]
 
 
-for i in range(N):
+for i in range(1, N):
+    for j in range(0, i):
+        a = a_ary[i] - a_ary[j]
+        for m in range(1, N):
+            for n in range(0, m):
+                b = b_ary[m] - b_ary[n]
+                if a + b == T:
+                    res += 1
+
+print(res)
+
 
