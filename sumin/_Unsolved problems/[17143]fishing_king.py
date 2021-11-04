0a26eea3b0b5a6aeae72795d023284db1f3db3b2
Sumin Kang <gsmin2018@gmail.com>
2021-11-04 14:11:25 +0000

    백준 문풀 수정

--- sumin/_Unsolved problems/[17143]fishing_king.py
+++ sumin/_Unsolved problems/[17143]fishing_king.py
@@ -16,15 +16,38 @@ def fishing():
 
 
 def movnig_shark(r, c, s, d, z): # 속력, 방향, 크기
-    if d == 1: # Up
-        s = s % ((R-1)*2)
-    elif d == 2: # Down
-        s = s % ((R-1)*2)
-    elif d == 3: # Right
-        s = s % ((C-1)*2)
+    nr, nc, nd = r, c, d
+     if d == 1: # Up (Row, Negative)
+        if s < r:
+            nr = r - s
+        else:
+            nr = (s - r) % (2*R - 2)
+            if nr >= R:
+                nr = 2*R - 2 - nr
+            nd = 2 if int((s - r)/(R - 1)) % 2 == 0 else 1
+    elif d == 2: # Down (Row, Positive)
+        nr = (r + s) % (2*R - 2)
+        if nr >= R:
+            nr = 2*R - 2 - nr
+        nd = 2 if int((r + s)/(R - 1)) % 2 == 0 else 1
+    elif d == 3: # Right (Column, Positive)
+        nc = (c + s) % (2*C - 2)
+        if nc >= C:
+            nc = 2*C - 2 - nc
+        nd = 3 if int((c + s)/(C - 1)) % 2 == 0 else 4
+    else: # d == 4, Left (Column, Negative)
+        if s < c:
+            nc = c - s
+        else:
+            nc = (s - c) % (2*C - 2)
+            if nc >= C:
+                nc = 2*C - 2 - nc
+            nd = 3 if int((s - c)/(C - 1)) % 2 == 0 else 4
+    return (nr, nc, s, nd, z)
 
-    elif d == 4: # Left
-        s = s % ((C-1)*2)
-    return (r, c, s, d, z)
 
-
+def set_graph(point_lst):
+    temp_lst = []
+    for r, c, s, d, z in point_lst:
+        temp_lst.append(movnig_shark(r, c, s, d, z))
+        
