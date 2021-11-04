b6704a9e75c41bedcbdfd56266977daeaf04c6c1
Sumin Kang <gsmin2018@gmail.com>
2021-11-04 15:01:23 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[2887]planet_tunnel.py
+++ sumin/_Unsolved problems/[2887]planet_tunnel.py
@@ -3,7 +3,6 @@ import heapq
 
 input = sys.stdin.readline
 
-
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -21,36 +20,24 @@ def union_parent(parent, a, b):
 
 def kruskal(edge):
     weight = 0
-    for i in range(E):
-        if find_parent(parent, edge[i][1]) != find_parent(parent, edge[i][2]):
-            union_parent(parent, edge[i][1], edge[i][2])
-            weight += edge[i][0]
+    for i in edge:
+        a, b = i[1], i[2]
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            weight += i[0]
     return weight
 
-def combi(ind, cnt):
-    global edge, E
-    if cnt == 2:
-        if lst[0] != lst[1]:
-            s1 = dic[lst[0]]
-            s2 = dic[lst[1]]
-            dis = min(abs(s1[0]-s2[0]), abs(s1[1]-s2[1]), abs(s1[2]-s2[2]))
-            heapq.heappush(edge, [dis, lst[0], lst[1]])
-            E += 1
-        return
-    for i in range(ind, N+1):
-        lst.append(i)
-        combi(ind+1, cnt+1)
-        lst.pop()
-
 N = int(input())
-edge = []
-lst = []
-parent = [x for x in range(N+1)]
-dic = {}
-E = 0
+planet_lst = list(list(map(int, input().split())) for _ in range(N))
+parent = [x for x in range(N)]
+weight = 0
 
-for i in range(1, N+1):
-    x, y, z = map(int, input().split())
-    dic[i] = [x, y, z]
-combi(1, 0)
-print(kruskal(edge))
+for i in range(N):
+    x1, y1, z1 = planet_lst[i]
+    for j in range(i+1, N):
+        x2, y2, z2 = planet_lst[j]
+        a, b = i, j
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            weight += min(abs(x1-x2), abs(y1-y2), abs(z1-z2))
+print(weight)
