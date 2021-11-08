f4e0242d58795eb2d5b476f9d9d80777ba683414
Sumin Kang <gsmin2018@gmail.com>
2021-11-08 15:02:08 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[16724]the_pied_piper_of_hamelin.py
+++ sumin/_Unsolved problems/[16724]the_pied_piper_of_hamelin.py
@@ -8,29 +8,40 @@ def union(a, b):
     n2 = find(b, parent)
     if n1 > n2:
         parent[n1] = n2
-    else:
+        return n2
+    elif n1 < n2:
         parent[n2] = n1
-
+        return n1
+    return False
 
 direc = {"D": (1, 0), "U": (-1, 0), "R": (0, 1), "L": (0, -1)}
 N, M = map(int, input().split())
 parent = [x for x in range(N*M)]
 graph = [list(input()) for _ in range(N)]
 ans_set = set()
+set_graph = [[0]*M for _ in range(N)]
 
 for i in range(N*M):
     row = i // M
     col = i % M
-    dr, dc = direc[graph[row][col]]
-    direc_i = i+(M*dr)+dc
-    union(i, direc_i)
+    set_graph[row][col] = i
 
-for i in range(N*M):
-    row = i // M
-    col = i % M
-    graph[row][col] = parent[i]
+while True:
+    is_change = False
+    for i in range(N):
+        for j in range(M):
+            dr, dc = direc[graph[i][j]]
+            nr, nc = i+dr, j+dc
+            tmp = union(set_graph[i][j], set_graph[nr][nc])
+            if tmp is not False:
+                set_graph[i][j] = tmp
+                set_graph[nr][nc] = tmp
+                is_change = True
+    if not is_change:
+        break
 
-for i in graph:
+
+for i in set_graph:
     print(*i)
 
 
--- sumin/_Unsolved problems/[9527]counting_ones.py
+++ sumin/_Unsolved problems/[9527]counting_ones.py
@@ -1,1 +1,14 @@
 a, b = map(int, input().split())
+
+def function(N):
+    global remain
+    if N == 0: return 0
+    l = len(str(bin(N))) - 2
+    top = N-2**(l-1)+1
+    botton = (l-1) * 2**(l-2)
+    remain = function(N-2**(l-1))
+    return int(top+botton+remain)
+
+print(function(b)-function(a-1))
+
+
