4939e26ba45aad64e9c531e68791e76698119272
Sumin Kang <gsmin2018@gmail.com>
2021-11-09 14:56:38 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[1007]matching_vertor.py
+++ sumin/_Unsolved problems/[1007]matching_vertor.py
@@ -1,36 +1,26 @@
 import sys
+from itertools import permutations
 
 input = sys.stdin.readline
 
 T = int(input())
 
-
-def permutation(ind, cnt, lst):
-    global res
-    if cnt == 2:
-        print(lst)
-        a = point[lst[0]]
-        b = point[lst[1]]
-        cal = (((a[0]-b[0])**2) + ((a[1]-b[1])**2)) ** (0.5)
-        res = min(res, cal)
-        print(res)
-        return
-    for i in range(len_point):
-        if len(lst) > 0 and lst[0] == i:
-            continue
-        lst.append(i)
-        permutation(ind+1, cnt+1, lst)
-        lst.pop()
-
-
-def combi(ind, cnt):
-    if cnt == 
-
-
 for _ in range(T):
+    ans = 1e13
     N = int(input())
-    point = [list(map(int, input().split())) for _ in range(N)]
-    res = 200000 * 21
-    len_point = len(point)
-    permutation(0, 0, [])
-    print(res)
+    p_lst = [list(map(int, input().split())) for _ in range(N)]
+    per1 = list(permutations([x for x in range(N)], 2)) # n_P_2
+    for i, j in per1:
+        x1, y1 = p_lst[i]
+        x2, y2 = p_lst[j]
+        per2 = list(permutations([x for x in range(N) if not (x == i or x == j) ], 2))
+        for m, n in per2:
+            x3, y3 = p_lst[m]
+            x4, y4 = p_lst[n]
+            print(i, j, m, n)
+            vec1 = (x1-x2, y1-y2)
+            vec2 = (x3-x4, y3-y4)
+            added_vec = (vec1[0]+vec2[0], vec1[1]+vec2[1])
+            ans = min(ans, (((added_vec[0])**2)+((added_vec[1])**2))**(0.5))
+
+    print(ans)
--- sumin/_Unsolved problems/[9527]counting_ones.py
+++ sumin/_Unsolved problems/[9527]counting_ones.py
@@ -3,12 +3,12 @@ a, b = map(int, input().split())
 def function(N):
     global remain
     if N == 0: return 0
-    l = len(str(bin(N))) - 2
+    l = len(str(bin(N))) - 2  # 자릿수
     top = N-2**(l-1)+1
     botton = (l-1) * 2**(l-2)
     remain = function(N-2**(l-1))
     return int(top+botton+remain)
 
 print(function(b)-function(a-1))
-
+print(bin(15))
 
