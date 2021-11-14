8844b3812438a4654b204a320857c60a2c8a3af9
Sumin Kang <gsmin2018@gmail.com>
2021-11-14 14:45:27 +0000

    백준 문풀 추가

--- sumin/_Unsolved problems/[4796]camping.py
+++ sumin/_Unsolved problems/[4796]camping.py
@@ -1,5 +1,8 @@
-n=int(input())
-lst=[int(input()) for _ in range(n)]
-lst.sort(reverse=True)
-for i in lst:
-    print(i)
+def turn_stat(order):
+    global stat
+    p_lst = [order, order+10, order-10, order+1, order-1]
+    for o in p_lst:
+        if 0 <= o < 100:
+            stat ^= (1<<o)
+
+
